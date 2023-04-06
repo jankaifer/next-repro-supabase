1074e188fb690319d8dec64557a131020e8f4592
Jan Kaifer <jan@kaifer.cz>
2023-04-06 23:46:02 +0200

    add supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  createBrowserSupabaseClient;
+  return null;
 }
